a53b21301a21716271b4719ba14a007540b078c3
SagarOP <sagar@zentrumhub.com>
2025-02-21 22:56:09 +0530

    fixes Link link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
   return (
 
     <div className='w-full bg-secondary flex items-center h-20'>
-      <Link href={`${isGhDeployment ? basePath : ''}/`} className='w-40 ml-2 mr-6 shrink-0 rounded overflow-hidden'>
+      <Link href={`/`} className='w-40 ml-2 mr-6 shrink-0 rounded overflow-hidden'>
         <img className='w-full object-scale-down' src={`${isGhDeployment ? basePath : ''}/images/tawaanzo-logo.jpg`} alt="Tawaanzo" />
       </Link>
 
