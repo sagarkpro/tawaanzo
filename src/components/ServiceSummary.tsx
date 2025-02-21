009e0d135b83a56f0cf956e564f9557ea99b6fe3
SagarOP <sagar@zentrumhub.com>
2025-02-21 23:21:09 +0530

    fixed href for serviceSummary

--- src/components/ServiceSummary.tsx
+++ src/components/ServiceSummary.tsx
@@ -6,7 +6,7 @@ export default function ServiceSummary(props: { service: ServiceSummaryInterface
   const isGhDeployment: boolean = process.env.NODE_ENV === "production";
   return (
     <div className='rounded-xl p-4 w-full shadow-[0_0px_7px_rgba(0,0,0,0.4)] h-full flex flex-col'>
-      <a className='text-xl font-medium px-2 text-primary hover:text-tint hover:underline' href={props.service.href}>{props.service.title}</a>
+      <a className='text-xl font-medium px-2 text-primary hover:text-tint hover:underline' href={`${isGhDeployment ? basePath : ''}/${props.service.href}`}>{props.service.title}</a>
       <p className='text-justify px-2 mt-4'>{props.service.description}</p>
       <ul className='list-disc p-6'>
         {
